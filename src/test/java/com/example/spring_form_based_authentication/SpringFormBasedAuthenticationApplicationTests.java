eac172260ef72fbbd9015346ea9f02b66f7f1d71
Vikram Verma <vikram.verma@collaberatact.com>
2025-06-11 20:40:05 +0530

    first commit

---
+++ src/test/java/com/example/spring_form_based_authentication/SpringFormBasedAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_form_based_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFormBasedAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
